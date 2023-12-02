4e74817c55b8b3e77c7c998a5192b183a5dd7ad9
Miguel <r315miguel@gmail.com>
2023-12-02 15:01:25 +0000

    app_org working

--- app_org/profile.py
+++ app_org/profile.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash, Flask
 from flask_login import login_required, current_user    
-from .models import User
-from . import db
+from app_org.models import User
+from app_org import db
 import os
 from werkzeug.security import generate_password_hash
 
